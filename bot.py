21f22515840abfd6b6c032e414ff137c43d33ece
Timo Martikainen <mail@timomartikainen.fi>
2026-03-25 14:58:57 +0200

    Moving/adding keys as constants

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import json
 import logging
+import utils.constants as constants
 
 from db import DatabaseManager
+#from utils.constants import MUSICAL_KEYS
 
 logger = logging.getLogger(__name__)
 
@@ -9,23 +11,13 @@ logger = logging.getLogger(__name__)
 with open("classicalbot.conf") as f:
     config = json.load(f)
 
-
 # lists works having a key on the title but no attribute set for it
 # and aren't part of another work or aren't arrangements
 def works_without_key():
 
-    keys = ["c-flat major", "c-flat minor", "c major", "c minor", "c-sharp major", "c-sharp minor",
-        "d-flat major", "d-flat minor", "d major", "d minor", "d-sharp major", "d-sharp minor",
-        "e-flat major", "e-flat minor", "e major", "e minor", "e-sharp major", "e-sharp minor",
-        "f-flat major", "f-flat minor", "f major", "f minor", "f-sharp major", "f-sharp minor",
-        "g-flat major", "g-flat minor", "g major", "g minor", "g-sharp major", "g-sharp minor",
-        "a-flat major", "a-flat minor", "a major", "a minor", "a-sharp major", "a-sharp minor",
-        "b-flat major", "b-flat minor", "b major", "b minor", "b-sharp major", "b-sharp minor"
-    ]
-
     # space is included to prevent "d major" matching with "mid major" or "end major"
     # work titles don't typically start with a key so this shouldn't be a major problem
-    search_keys = [f"% {key}%" for key in keys]
+    search_keys = [f"% {key}%" for key in constants.MUSICAL_KEYS_EN]
 
     with DatabaseManager(config['database_mb']) as db:
 
@@ -38,9 +30,9 @@ def works_without_key():
               AND NOT EXISTS (
                   SELECT 1
                   FROM work_attribute wa
-                  JOIN work_attribute_type waty ON wa.work_attribute_type = waty.id
+                  JOIN work_attribute_type wat ON wa.work_attribute_type = wat.id
                   WHERE wa.work = w.id
-                    AND waty.gid = '7526c19d-3be4-3420-b6cc-9fb6e49fa1a9' -- key
+                    AND wat.gid = '7526c19d-3be4-3420-b6cc-9fb6e49fa1a9' -- key
               )
 
               -- 2. not a work part/movement or an arrangement
@@ -52,7 +44,6 @@ def works_without_key():
                   WHERE lww.entity1 = w.id
                     AND (lt.gid = 'ca8d3642-ce5f-49f8-91f2-125d72524e6a'    -- part
                         OR lt.gid = '51975ed8-bbfa-486b-9f28-5947f4370299') -- arrangement
-
               )
         """
 
---
+++ utils/constants.py
@@ -0,0 +1,37 @@
+# keys stored in MB also include some theoretical oddities
+
+MUSICAL_KEYS_EN = ["C-flat major", "C-flat minor", "C major", "C minor", "C-sharp major", "C-sharp minor",
+    "D-flat major", "D-flat minor", "D major", "D minor", "D-sharp major", "D-sharp minor",
+    "E-flat major", "E-flat minor", "E major", "E minor", "E-sharp major", "E-sharp minor",
+    "F-flat major", "F-flat minor", "F major", "F minor", "F-sharp major", "F-sharp minor",
+    "G-flat major", "G-flat minor", "G major", "G minor", "G-sharp major", "G-sharp minor",
+    "A-flat major", "A-flat minor", "A major", "A minor", "A-sharp major", "A-sharp minor",
+    "B-flat major", "B-flat minor", "B major", "B minor", "B-sharp major", "B-sharp minor"
+]
+
+MUSICAL_KEYS_DE = ["Ces-Dur", "ces-Moll", "C-Dur", "c-Moll", "Cis-Dur", "cis-Moll",
+    "Des-Dur", "des-Moll", "D-Dur", "d-Moll", "Dis-Dur", "dis-Moll",
+    "Es-Dur", "es-Moll", "E-Moll", "e-Moll", "Eis-Dur", "eis-Moll",
+    "Fes-Dur", "fes-Moll", "F-Dur", "f-Moll", "Fis-Dur", "fis-Moll",
+    "Ges-Dur", "ges-Moll", "G-Dur", "g-Moll", "Gis-Dur", "gis-Moll",
+    "As-Dur", "as-Moll", "A-Dur", "a-Moll", "Ais-Dur", "ais-Moll",
+    "B-Dur", "b-Moll", "H-Dur", "h-Moll", "bis-Dur", "bis-Moll"
+]
+
+MUSICAL_KEYS_FR = ["Do bémol majeur", "Do bémol mineur", "Do majeur", "Do mineur", "Do dièse majeur", "Do dièse mineur",
+    "Ré bémol majeur", "Ré bémol mineur", "Ré majeur", "Ré mineur", "Ré dièse majeur", "Ré dièse mineur",
+    "Mi bémol majeur", "Mi bémol mineur", "Mi majeur", "Mi mineur", "Mi dièse majeur", "Mi dièse mineur",
+    "Fa bémol majeur", "Fa bémol mineur", "Fa majeur", "Fa mineur", "Fa dièse majeur", "Fa dièse mineur",
+    "Sol bémol majeur", "Sol bémol mineur", "Sol majeur", "Sol mineur", "Sol dièse majeur", "Sol dièse mineur",
+    "La bémol mineur", "La bémol mineur", "La majeur", "La mineur", "La dièse majeur", "La dièse mineur",
+    "Si bémol majeur", "Si bémol mineur", "Si majeur", "Si mineur", "Si dièse majeur", "Si dièse mineur"
+]
+
+MUSICAL_KEYS_FI = ["Ces-duuri", "Ces-molli", "C-duuri", "C-molli", "Cis-duuri", "Cis-molli",
+    "Des-duuri", "Des-molli", "D-duuri", "D-molli", "Dis-duuri", "Dis-molli",
+    "Es-duuri", "Es-molli", "E-duuri", "E-molli", "Eis-duuri", "Eis-molli",
+    "Fes-duuri", "Fes-molli", "F-duuri", "F-molli", "Fis-duuri", "Fis-molli",
+    "Ges-duuri", "Ges-molli", "G-duuri", "G-molli", "Gis-duuri", "Gis-molli",
+    "As-duuri", "As-molli", "A-duuri", "A-molli", "Ais-duuri", "Ais-molli",
+    "B-duuri", "B-molli", "H-duuri", "H-molli", "His-duuri", "His-molli",
+]
